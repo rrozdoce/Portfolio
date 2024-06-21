90354edb2bb002169cac5519e73ccac813b3678d
rrozdoce <felipefelipevilhalva@gmail.com>
2024-06-20 23:35:17 -0400

    .

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
+import Terminal from './components/Terminal.vue'
 </script>
 
-<template>
+<template >
+    <Terminal></Terminal>
 </template>
 
 <style scoped>
+html, body, #app {
+  height: 100%;
+  margin: 0;
+}
 </style>
---
+++ src/components/Terminal.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template class="flex items-center justify-center min-h-screen w-full h-full">
+    <div class="bg-custom-dark text-white p-4 rounded-lg">
+        Esta é uma div colorida com Tailwind CSS!
+    </div>
+</template>
+
+<style scoped>
+</style>
